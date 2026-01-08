3c73e5e27117200cb7a1adc51865bd2efa9a8d0a
huynhhoang04 <huynh696k@gmail.com>
2026-01-08 20:27:57 +0700

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/productmanagement";
+    private static final String USER = "postgres";
+    private static final String PASS = "123456";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
